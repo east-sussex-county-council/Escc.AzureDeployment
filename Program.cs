df9a4a226807a5dc0aad3f7d0a8dafe9c7a954a9
richardm <richard.mason@eastsussex.gov.uk>
2014-02-05 17:36:35 +0000

    Add transform code

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Escc.EastSussexGovUK.AzureDeployment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var configFile = new XmlDocument();
+            configFile.Load(args[0]);
+            var node = configFile.SelectSingleNode("/configuration/CustomSection/add[@key='CustomConfig']/@value");
+            if (node != null)
+            {
+                node.Value = Environment.GetEnvironmentVariable(node.Value);
+            }
+
+            configFile.Save(args[0]);
+        }
+    }
+}
