490b434437c34c9f49754b3f907b64b857a957d4
richardm <richard.mason@eastsussex.gov.uk>
2014-02-06 17:30:23 +0000

    Use XSLT to change strong name

--- Escc.EastSussexGovUK.AzureDeployment.Kudu/BuildApplication.cmd
+++ Escc.EastSussexGovUK.AzureDeployment.Kudu/BuildApplication.cmd
@@ -11,6 +11,8 @@ echo Building %1
 echo ------------------------------------------------------
 echo.
 
+"%STRONG_NAME_PATH%\msxsl" %1 "%STRONG_NAME_PATH%\replace-strong-name.xslt" 
+
 IF /I "%IN_PLACE_DEPLOYMENT%" NEQ "1" (
   %MSBUILD_PATH% %1 /nologo /verbosity:m /t:Build /t:pipelinePreDeployCopyAllFilesToOneFolder /p:_PackageTempDir="%DEPLOYMENT_TEMP%";AutoParameterizationWebConfigConnectionStrings=false;Configuration=Release /p:SolutionDir="%DEPLOYMENT_SOURCE%\.\\" %SCM_BUILD_ARGS%
 ) ELSE (
