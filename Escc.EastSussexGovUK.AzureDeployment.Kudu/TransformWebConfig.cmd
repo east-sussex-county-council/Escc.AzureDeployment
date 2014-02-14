3014c44d8a298049701fbd9f6b7d90a7c90e343c
richardm <richard.mason@eastsussex.gov.uk>
2014-02-14 14:16:30 +0000

    Encrypt appSettings and connectionStrings

--- Escc.EastSussexGovUK.AzureDeployment.Kudu/TransformWebConfig.cmd
+++ Escc.EastSussexGovUK.AzureDeployment.Kudu/TransformWebConfig.cmd
@@ -13,6 +13,9 @@ echo.
 
 if exist "%DEPLOYMENT_SOURCE%\%1\web.config.example" (                                 
   %MSBUILD_PATH% "%ESCC_DEPLOYMENT_SCRIPTS%\Escc.EastSussexGovUK.AzureDeployment.Kudu\TransformWebConfig.xml" /p:TransformInputFile="%DEPLOYMENT_SOURCE%\%1\web.config.example" /p:TransformFile="%DEPLOYMENT_CONFIG_TRANSFORMS%\%1\Web.config.Release" /p:TransformOutputFile="%DEPLOYMENT_TARGET%\%1\web.config"
+
+  aspnet_regiis -pef appSettings ""%DEPLOYMENT_TARGET%\%1"
+  aspnet_regiis -pef connectionStrings ""%DEPLOYMENT_TARGET%\%1"
 )
 
 :exit
