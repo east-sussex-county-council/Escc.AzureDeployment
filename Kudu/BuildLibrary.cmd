f2bec7826d37bcfe5fc4d6f2645aee83c2853d37
richardm <richard.mason@eastsussex.gov.uk>
2014-04-15 10:46:07 +0100

    Change parameters of BuildLibrary to separate project file and folder

--- Kudu/BuildLibrary.cmd
+++ Kudu/BuildLibrary.cmd
@@ -1,34 +1,36 @@
 @if "%SCM_TRACE_LEVEL%" NEQ "4" @echo off
 
-if String.Empty%1==String.Empty (
-	echo Usage: BuildLibrary ^<relative path to .csproj file^>
+if "%1"=="" (
+	echo Usage: BuildLibrary ^<relative path to project folder^> ^<.csproj file^>
+	goto exit
+)
+
+if "%2"=="" (
+	echo Usage: BuildLibrary ^<relative path to project folder^> ^<.csproj file^>
 	goto exit
 )
 
 echo.
 echo ------------------------------------------------------
-echo Building %1
+echo Building %1%2
 echo ------------------------------------------------------
 echo.
 
-if not exist "%1.xslt" (
-  %MSBUILD_PATH% "%ESCC_DEPLOYMENT_SCRIPTS%\TransformProjectFile.xml" /p:ProjectFile=%1 /p:TransformFile="%ESCC_DEPLOYMENT_SCRIPTS%\TransformProjectFile.xslt" /p:StrongNamePath="%DEPLOYMENT_STRONG_NAME_KEY%"
+if not exist "%1%2.xslt" (
+  %MSBUILD_PATH% "%ESCC_DEPLOYMENT_SCRIPTS%\TransformProjectFile.xml" /p:ProjectFile="%1%2" /p:TransformFile="%ESCC_DEPLOYMENT_SCRIPTS%\TransformProjectFile.xslt" /p:StrongNamePath="%DEPLOYMENT_STRONG_NAME_KEY%"
 )
 
-if exist "%1.xslt" (
-  for %%A in (%1) do (
-     set PROJECT_FOLDER=%%~dpA
-  )
-  echo copy "%ESCC_DEPLOYMENT_SCRIPTS%\TransformProjectFile.xslt" %PROJECT_FOLDER%
-  copy "%ESCC_DEPLOYMENT_SCRIPTS%\TransformProjectFile.xslt" %PROJECT_FOLDER%
+if exist "%1%2.xslt" (
+  echo copy "%ESCC_DEPLOYMENT_SCRIPTS%\TransformProjectFile.xslt" "%1"
+  copy "%ESCC_DEPLOYMENT_SCRIPTS%\TransformProjectFile.xslt" "%1"
  
-  %MSBUILD_PATH% "%ESCC_DEPLOYMENT_SCRIPTS%\TransformProjectFile.xml" /p:ProjectFile=%1 /p:TransformFile="%1.xslt" /p:ReferenceDllPath="%DEPLOYMENT_TRANSFORMS%" /p:StrongNamePath="%DEPLOYMENT_STRONG_NAME_KEY%"
+  %MSBUILD_PATH% "%ESCC_DEPLOYMENT_SCRIPTS%\TransformProjectFile.xml" /p:ProjectFile="%1%2" /p:TransformFile="%1%2.xslt" /p:ReferenceDllPath="%DEPLOYMENT_TRANSFORMS%" /p:StrongNamePath="%DEPLOYMENT_STRONG_NAME_KEY%"
 )
 
 IF /I "%IN_PLACE_DEPLOYMENT%" NEQ "1" (
-  %MSBUILD_PATH% %1 /nologo /verbosity:m /t:Build /p:_PackageTempDir="%DEPLOYMENT_TEMP%";AutoParameterizationWebConfigConnectionStrings=false;Configuration=Release /p:SolutionDir="%DEPLOYMENT_SOURCE%\.\\" %SCM_BUILD_ARGS%
+  %MSBUILD_PATH% "%1%2" /nologo /verbosity:m /t:Build /p:_PackageTempDir="%DEPLOYMENT_TEMP%";AutoParameterizationWebConfigConnectionStrings=false;Configuration=Release /p:SolutionDir="%DEPLOYMENT_SOURCE%\.\\" %SCM_BUILD_ARGS%
 ) ELSE (
-  %MSBUILD_PATH% %1 /nologo /verbosity:m /t:Build /p:AutoParameterizationWebConfigConnectionStrings=false;Configuration=Release /p:SolutionDir="%DEPLOYMENT_SOURCE%\.\\" %SCM_BUILD_ARGS%
+  %MSBUILD_PATH% "%1%2" /nologo /verbosity:m /t:Build /p:AutoParameterizationWebConfigConnectionStrings=false;Configuration=Release /p:SolutionDir="%DEPLOYMENT_SOURCE%\.\\" %SCM_BUILD_ARGS%
 )
 
 :exit
