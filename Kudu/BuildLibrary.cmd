464056607f96ac14b534d468a9716bbf29eda996
richardm <richard.mason@eastsussex.gov.uk>
2014-04-15 14:41:52 +0100

    Remove quotes from XSL parameters where they cause problems. Restore others.

--- Kudu/BuildLibrary.cmd
+++ Kudu/BuildLibrary.cmd
@@ -17,14 +17,14 @@ echo ------------------------------------------------------
 echo.
 
 if not exist "%1%2.xslt" (
-  %MSBUILD_PATH% "%ESCC_DEPLOYMENT_SCRIPTS%\TransformProjectFile.xml" /p:ProjectFile=%1%2 /p:TransformFile=%ESCC_DEPLOYMENT_SCRIPTS%\TransformProjectFile.xslt /p:StrongNamePath=%DEPLOYMENT_STRONG_NAME_KEY%
+  %MSBUILD_PATH% "%ESCC_DEPLOYMENT_SCRIPTS%\TransformProjectFile.xml" /p:ProjectFile="%1%2" /p:TransformFile="%ESCC_DEPLOYMENT_SCRIPTS%\TransformProjectFile.xslt" /p:StrongNamePath=%DEPLOYMENT_STRONG_NAME_KEY%
 )
 
 if exist "%1%2.xslt" (
   echo copy "%ESCC_DEPLOYMENT_SCRIPTS%\TransformProjectFile.xslt" "%1"
   copy "%ESCC_DEPLOYMENT_SCRIPTS%\TransformProjectFile.xslt" "%1"
  
-  %MSBUILD_PATH% "%ESCC_DEPLOYMENT_SCRIPTS%\TransformProjectFile.xml" /p:ProjectFile=%1%2 /p:TransformFile=%1%2.xslt /p:ReferenceDllPath="%DEPLOYMENT_TRANSFORMS%" /p:StrongNamePath=%DEPLOYMENT_STRONG_NAME_KEY%
+  %MSBUILD_PATH% "%ESCC_DEPLOYMENT_SCRIPTS%\TransformProjectFile.xml" /p:ProjectFile="%1%2" /p:TransformFile="%1%2.xslt" /p:ReferenceDllPath=%DEPLOYMENT_TRANSFORMS% /p:StrongNamePath=%DEPLOYMENT_STRONG_NAME_KEY%
 )
 
 IF /I "%IN_PLACE_DEPLOYMENT%" NEQ "1" (
