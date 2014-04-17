312b27fb6db64865617383e46606600d410fe0f8
richardm <richard.mason@eastsussex.gov.uk>
2014-04-17 16:08:19 +0100

    Test skipping the build of libraries, and allowing them to be built only when referenced

--- Kudu/BuildLibrary.cmd
+++ Kudu/BuildLibrary.cmd
@@ -27,11 +27,5 @@ if exist "%1%2.xslt" (
   %MSBUILD_PATH% "%ESCC_DEPLOYMENT_SCRIPTS%\TransformProjectFile.xml" /p:ProjectFile="%1%2" /p:TransformFile="%1%2.xslt" /p:ReferenceDllPath=%DEPLOYMENT_TRANSFORMS% /p:StrongNamePath=%DEPLOYMENT_STRONG_NAME_KEY%
 )
 
-IF /I "%IN_PLACE_DEPLOYMENT%" NEQ "1" (
-  %MSBUILD_PATH% "%1%2" /nologo /verbosity:m /t:Build /p:_PackageTempDir="%DEPLOYMENT_TEMP%";AutoParameterizationWebConfigConnectionStrings=false;Configuration=Release /p:SolutionDir="%DEPLOYMENT_SOURCE%\.\\" %SCM_BUILD_ARGS%
-) ELSE (
-  %MSBUILD_PATH% "%1%2" /nologo /verbosity:m /t:Build /p:AutoParameterizationWebConfigConnectionStrings=false;Configuration=Release /p:SolutionDir="%DEPLOYMENT_SOURCE%\.\\" %SCM_BUILD_ARGS%
-)
-
 :exit
 exit /b %ERRORLEVEL%
