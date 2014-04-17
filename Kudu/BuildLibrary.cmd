2ec838900d6242d3896806cbe356d3ad041a3fbf
richardm <richard.mason@eastsussex.gov.uk>
2014-04-17 16:30:37 +0100

    Move commands to transform project file into a separate script

--- Kudu/BuildLibrary.cmd
+++ Kudu/BuildLibrary.cmd
@@ -10,21 +10,18 @@ if "%2"=="" (
 	goto exit
 )
 
+call TransformProjectFile %1 %2
+
 echo.
 echo ------------------------------------------------------
 echo Building %1%2
 echo ------------------------------------------------------
 echo.
 
-if not exist "%1%2.xslt" (
-  %MSBUILD_PATH% "%ESCC_DEPLOYMENT_SCRIPTS%\TransformProjectFile.xml" /p:ProjectFile="%1%2" /p:TransformFile="%ESCC_DEPLOYMENT_SCRIPTS%\TransformProjectFile.xslt" /p:StrongNamePath=%DEPLOYMENT_STRONG_NAME_KEY%
-)
-
-if exist "%1%2.xslt" (
-  echo copy "%ESCC_DEPLOYMENT_SCRIPTS%\TransformProjectFile.xslt" "%1"
-  copy "%ESCC_DEPLOYMENT_SCRIPTS%\TransformProjectFile.xslt" "%1"
- 
-  %MSBUILD_PATH% "%ESCC_DEPLOYMENT_SCRIPTS%\TransformProjectFile.xml" /p:ProjectFile="%1%2" /p:TransformFile="%1%2.xslt" /p:ReferenceDllPath=%DEPLOYMENT_TRANSFORMS% /p:StrongNamePath=%DEPLOYMENT_STRONG_NAME_KEY%
+IF /I "%IN_PLACE_DEPLOYMENT%" NEQ "1" (
+  %MSBUILD_PATH% "%1%2" /nologo /verbosity:m /t:Build /p:_PackageTempDir="%DEPLOYMENT_TEMP%";AutoParameterizationWebConfigConnectionStrings=false;Configuration=Release /p:SolutionDir="%DEPLOYMENT_SOURCE%\.\\" %SCM_BUILD_ARGS%
+) ELSE (
+  %MSBUILD_PATH% "%1%2" /nologo /verbosity:m /t:Build /p:AutoParameterizationWebConfigConnectionStrings=false;Configuration=Release /p:SolutionDir="%DEPLOYMENT_SOURCE%\.\\" %SCM_BUILD_ARGS%
 )
 
 :exit
---
+++ Kudu/TransformProjectFile.cmd
@@ -0,0 +1,31 @@
+@if "%SCM_TRACE_LEVEL%" NEQ "4" @echo off
+
+if "%1"=="" (
+	echo Usage: TransformProjectFile ^<relative path to project folder^> ^<.csproj file^>
+	goto exit
+)
+
+if "%2"=="" (
+	echo Usage: TransformProjectFile ^<relative path to project folder^> ^<.csproj file^>
+	goto exit
+)
+
+echo.
+echo ------------------------------------------------------
+echo Transforming %1%2
+echo ------------------------------------------------------
+echo.
+
+if not exist "%1%2.xslt" (
+  %MSBUILD_PATH% "%ESCC_DEPLOYMENT_SCRIPTS%\TransformProjectFile.xml" /p:ProjectFile="%1%2" /p:TransformFile="%ESCC_DEPLOYMENT_SCRIPTS%\TransformProjectFile.xslt" /p:StrongNamePath=%DEPLOYMENT_STRONG_NAME_KEY%
+)
+
+if exist "%1%2.xslt" (
+  echo copy "%ESCC_DEPLOYMENT_SCRIPTS%\TransformProjectFile.xslt" "%1"
+  copy "%ESCC_DEPLOYMENT_SCRIPTS%\TransformProjectFile.xslt" "%1"
+ 
+  %MSBUILD_PATH% "%ESCC_DEPLOYMENT_SCRIPTS%\TransformProjectFile.xml" /p:ProjectFile="%1%2" /p:TransformFile="%1%2.xslt" /p:ReferenceDllPath=%DEPLOYMENT_TRANSFORMS% /p:StrongNamePath=%DEPLOYMENT_STRONG_NAME_KEY%
+)
+
+:exit
+exit /b %ERRORLEVEL%
