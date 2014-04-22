2a62bd0826c6c6727c90e9398ef0c6322e0cec1c
richardm <richard.mason@eastsussex.gov.uk>
2014-04-22 12:42:19 +0100

    Remove extra backslash from paths

--- Kudu/TransformWebConfig.cmd
+++ Kudu/TransformWebConfig.cmd
@@ -11,22 +11,22 @@ echo Transforming %1\web.example.config
 echo ------------------------------------------------------
 echo.
 
-if exist "%DEPLOYMENT_TRANSFORMS%\%1\Web.Release.config" (
+if exist "%DEPLOYMENT_TRANSFORMS%%1\Web.Release.config" (
 
   if exist "%DEPLOYMENT_TARGET%\%1\web.config" (                                 
-    %MSBUILD_PATH% "%ESCC_DEPLOYMENT_SCRIPTS%\TransformWebConfig.xml" /p:TransformInputFile="%DEPLOYMENT_TARGET%\%1\web.config" /p:TransformFile="%DEPLOYMENT_TRANSFORMS%\%1\Web.Release.config" /p:TransformOutputFile="%DEPLOYMENT_TARGET%\%1\web.config"
+    %MSBUILD_PATH% "%ESCC_DEPLOYMENT_SCRIPTS%\TransformWebConfig.xml" /p:TransformInputFile="%DEPLOYMENT_TARGET%\%1\web.config" /p:TransformFile="%DEPLOYMENT_TRANSFORMS%%1\Web.Release.config" /p:TransformOutputFile="%DEPLOYMENT_TARGET%\%1\web.config"
   )
   
   if not exist "%DEPLOYMENT_TARGET%\%1\web.config" (
     if exist "%DEPLOYMENT_SOURCE%\%1\web.example.config" (
-      %MSBUILD_PATH% "%ESCC_DEPLOYMENT_SCRIPTS%\TransformWebConfig.xml" /p:TransformInputFile="%DEPLOYMENT_SOURCE%\%1\web.example.config" /p:TransformFile="%DEPLOYMENT_TRANSFORMS%\%1\Web.Release.config" /p:TransformOutputFile="%DEPLOYMENT_TARGET%\%1\web.config"
+      %MSBUILD_PATH% "%ESCC_DEPLOYMENT_SCRIPTS%\TransformWebConfig.xml" /p:TransformInputFile="%DEPLOYMENT_SOURCE%\%1\web.example.config" /p:TransformFile="%DEPLOYMENT_TRANSFORMS%%1\Web.Release.config" /p:TransformOutputFile="%DEPLOYMENT_TARGET%\%1\web.config"
       
     )
   )      
 )
 
-if not exist "%DEPLOYMENT_TRANSFORMS%\%1\Web.Release.config" (
-  echo %DEPLOYMENT_TRANSFORMS%\%1\Web.Release.config not found.
+if not exist "%DEPLOYMENT_TRANSFORMS%%1\Web.Release.config" (
+  echo %DEPLOYMENT_TRANSFORMS%%1\Web.Release.config not found.
 )
 
 :exit
