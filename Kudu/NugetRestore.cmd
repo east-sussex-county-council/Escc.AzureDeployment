57e38a7d55cb2bd94cc8acc0e8131742a1dec495
richardm <richard.mason@eastsussex.gov.uk>
2014-07-11 16:27:06 +0100

    Replace old environment variable with %1

--- Kudu/NugetRestore.cmd
+++ Kudu/NugetRestore.cmd
@@ -14,15 +14,15 @@ echo.
 IF /I "%1" NEQ "" (
 
   :: When the solution file is elsewhere, restore from packages.config next to the .csproj by specifying the folder
-  if exist "%NUGET_RESTORE_FROM%\packages.config" (
-      call "%NUGET_EXE%" restore "%NUGET_RESTORE_FROM%\packages.config" -OutputDirectory "%NUGET_RESTORE_FROM%\packages" -NonInteractive
+  if exist "%1\packages.config" (
+      call "%NUGET_EXE%" restore "%1\packages.config" -OutputDirectory "%1\packages" -NonInteractive
       goto exit
   ) 
   
-  if not exist "%NUGET_RESTORE_FROM%\packages.config" (
+  if not exist "%1\packages.config" (
       :: Otherwise assume parameter was a solution file, useful when the solution file is in the same folder as the project file being built.
       :: Syntax like if /I "%NUGET_RESTORE_FROM:~-4%"==".sln" to check value was invalid on Kudu.
-      call "%NUGET_EXE%" restore %NUGET_RESTORE_FROM% -NonInteractive
+      call "%NUGET_EXE%" restore %1 -NonInteractive
       goto exit
   )
 
