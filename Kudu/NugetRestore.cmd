a4ac79262919ae8c7a36fcf1afa73e3eab32e013
richardm <richard.mason@eastsussex.gov.uk>
2014-07-11 16:21:30 +0100

    Try different syntax for test, as original syntax seems invalid on Kudu

--- Kudu/NugetRestore.cmd
+++ Kudu/NugetRestore.cmd
@@ -1,7 +1,7 @@
 @if "%SCM_TRACE_LEVEL%" NEQ "4" @echo off
 
 if "%1"=="" (
-	echo Usage: NugetRestore ^<relative path to .sln file^>
+	echo Usage: NugetRestore ^<relative path to .sln file or folder containing packages.config file^>
 	goto exit
 )
 
@@ -13,11 +13,14 @@ echo.
 
 IF /I "%1" NEQ "" (
 
-  set NUGET_RESTORE_FROM=%1
-
-  :: Allow restore from a solution file, useful when the solution file is in the same folder as the project file being built
-  if /I "%NUGET_RESTORE_FROM:~-4%"==".sln"
-  (
+  :: When the solution file is elsewhere, restore from packages.config next to the .csproj by specifying the folder
+  if exist "%NUGET_RESTORE_FROM%\packages.config" (
+      call "%NUGET_EXE%" restore "%NUGET_RESTORE_FROM%\packages.config" -OutputDirectory "%NUGET_RESTORE_FROM%\packages" -NonInteractive
+      goto exit
+  ) 
+  else (
+      :: Otherwise assume parameter was a solution file, useful when the solution file is in the same folder as the project file being built.
+      :: Syntax like if /I "%NUGET_RESTORE_FROM:~-4%"==".sln" to check value was invalid on Kudu.
       call "%NUGET_EXE%" restore %NUGET_RESTORE_FROM% -NonInteractive
       goto exit
   )
