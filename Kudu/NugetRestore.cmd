0c20ce9dd1ea1c75921312f846f379bf4ebaafd9
richardm <richard.mason@eastsussex.gov.uk>
2014-07-11 15:27:18 +0100

    Update nuget command

--- Kudu/NugetRestore.cmd
+++ Kudu/NugetRestore.cmd
@@ -18,13 +18,15 @@ IF /I "%1" NEQ "" (
   :: Allow restore from a solution file, useful when the solution file is in the same folder as the project file being built
   if /I "%NUGET_RESTORE_FROM:~-4%"==".sln"
   (
-      call "%NUGET_EXE%" restore %NUGET_RESTORE_FROM%
+      echo "Nuget restore from %NUGET_RESTORE_FROM%
+      call "%NUGET_EXE%" restore %NUGET_RESTORE_FROM% -NonInteractive
       goto exit
   )
 
   :: When the solution file is elsewhere, restore from packages.config next to the .csproj by specifying the folder
   if exist "%NUGET_RESTORE_FROM%\packages.config" (
-      call "%NUGET_EXE%" install "%NUGET_RESTORE_FROM%\packages.config" -OutputDirectory "%NUGET_RESTORE_FROM%\packages" -NonInteractive
+      echo "Nuget restore from %NUGET_RESTORE_FROM%\packages.config
+      call "%NUGET_EXE%" restore "%NUGET_RESTORE_FROM%\packages.config" -OutputDirectory "%NUGET_RESTORE_FROM%\packages" -NonInteractive
       goto exit
   )
 
