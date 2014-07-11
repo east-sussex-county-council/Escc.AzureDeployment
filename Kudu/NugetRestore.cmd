88f2e2aed1e5e25bde7d608445cd39db5e317011
richardm <richard.mason@eastsussex.gov.uk>
2014-07-11 16:12:13 +0100

    Restore if statement to support original use case

--- Kudu/NugetRestore.cmd
+++ Kudu/NugetRestore.cmd
@@ -12,7 +12,16 @@ echo ------------------------------------------------------
 echo.
 
 IF /I "%1" NEQ "" (
-  call "%NUGET_EXE%" restore %1
+
+  set NUGET_RESTORE_FROM=%1
+
+  :: Allow restore from a solution file, useful when the solution file is in the same folder as the project file being built
+  if /I "%NUGET_RESTORE_FROM:~-4%"==".sln"
+  (
+      call "%NUGET_EXE%" restore %NUGET_RESTORE_FROM% -NonInteractive
+      goto exit
+  )
+
 )
 
 :exit
