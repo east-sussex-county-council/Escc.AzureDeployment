e0f1287607a5a10f45515537735fa1ddc6010732
Rick Mason <richard.mason@eastsussex.gov.uk>
2015-03-30 12:50:25 +0100

    Transform config as part of build rather than part of deployment

--- Kudu/TransformConfig.cmd
+++ Kudu/TransformConfig.cmd
@@ -15,20 +15,20 @@ echo.
 
 if exist "%DEPLOYMENT_TRANSFORMS%%1.Release.config" (
 
-  if exist "%DEPLOYMENT_TARGET%\%1.config" (
-    %MSBUILD_PATH% "%ESCC_DEPLOYMENT_SCRIPTS%\TransformConfig.xml" /p:TransformInputFile="%DEPLOYMENT_TARGET%\%1.config" /p:TransformFile="%DEPLOYMENT_TRANSFORMS%%1.Release.config" /p:TransformOutputFile="%DEPLOYMENT_TARGET%\%1.config"
+  if exist "%DEPLOYMENT_SOURCE%\%1.config" (
+    %MSBUILD_PATH% "%ESCC_DEPLOYMENT_SCRIPTS%\TransformConfig.xml" /p:TransformInputFile="%DEPLOYMENT_SOURCE%\%1.config" /p:TransformFile="%DEPLOYMENT_TRANSFORMS%%1.Release.config" /p:TransformOutputFile="%DEPLOYMENT_SOURCE%\%1.config"
   )
   
-  if not exist "%DEPLOYMENT_TARGET%\%1.config" (
+  if not exist "%DEPLOYMENT_SOURCE%\%1.config" (
     if exist "%DEPLOYMENT_SOURCE%\%1.example.config" (
-      %MSBUILD_PATH% "%ESCC_DEPLOYMENT_SCRIPTS%\TransformConfig.xml" /p:TransformInputFile="%DEPLOYMENT_SOURCE%\%1.example.config" /p:TransformFile="%DEPLOYMENT_TRANSFORMS%%1.Release.config" /p:TransformOutputFile="%DEPLOYMENT_TARGET%\%1.config"
+      %MSBUILD_PATH% "%ESCC_DEPLOYMENT_SCRIPTS%\TransformConfig.xml" /p:TransformInputFile="%DEPLOYMENT_SOURCE%\%1.example.config" /p:TransformFile="%DEPLOYMENT_TRANSFORMS%%1.Release.config" /p:TransformOutputFile="%DEPLOYMENT_SOURCE%\%1.config"
       
     )
   )      
 
   :: Delete temp file created by transformation, because deleting it within the transformation fails due to file locking 
-  if exist "%DEPLOYMENT_TARGET%\%1.config.temp.config" (
-    del "%DEPLOYMENT_TARGET%\%1.config.temp.config" 
+  if exist "%DEPLOYMENT_SOURCE%\%1.config.temp.config" (
+    del "%DEPLOYMENT_SOURCE%\%1.config.temp.config" 
   )
 )
 
