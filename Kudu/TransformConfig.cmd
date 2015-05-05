5b3ffdd75f3ac38f4e90927a47dcfa726439e9b6
Rick Mason <richard.mason@eastsussex.gov.uk>
2015-05-05 17:34:04 +0100

    Give web.example.config priority, as otherwise a redeploy behaves differently to a deploy

--- Kudu/TransformConfig.cmd
+++ Kudu/TransformConfig.cmd
@@ -15,18 +15,19 @@ echo.
 
 if exist "%DEPLOYMENT_TRANSFORMS%%1.Release.config" (
 
-  if exist "%DEPLOYMENT_SOURCE%\%1.config" (
-    %MSBUILD_PATH% "%ESCC_DEPLOYMENT_SCRIPTS%\TransformConfig.xml" /p:TransformInputFile="%DEPLOYMENT_SOURCE%\%1.config" /p:TransformFile="%DEPLOYMENT_TRANSFORMS%%1.Release.config" /p:TransformOutputFile="%DEPLOYMENT_SOURCE%\%1.config"
+  REM Look first for *.example.config
+  if exist "%DEPLOYMENT_SOURCE%\%1.example.config" (
+    %MSBUILD_PATH% "%ESCC_DEPLOYMENT_SCRIPTS%\TransformConfig.xml" /p:TransformInputFile="%DEPLOYMENT_SOURCE%\%1.example.config" /p:TransformFile="%DEPLOYMENT_TRANSFORMS%%1.Release.config" /p:TransformOutputFile="%DEPLOYMENT_SOURCE%\%1.config"
   )
   
-  if not exist "%DEPLOYMENT_SOURCE%\%1.config" (
-    if exist "%DEPLOYMENT_SOURCE%\%1.example.config" (
-      %MSBUILD_PATH% "%ESCC_DEPLOYMENT_SCRIPTS%\TransformConfig.xml" /p:TransformInputFile="%DEPLOYMENT_SOURCE%\%1.example.config" /p:TransformFile="%DEPLOYMENT_TRANSFORMS%%1.Release.config" /p:TransformOutputFile="%DEPLOYMENT_SOURCE%\%1.config"
-      
+  REM If that wasn't found, fall back to the possibility that web.config has been created some other way
+  if not exist "%DEPLOYMENT_SOURCE%\%1.example.config" (
+    if exist "%DEPLOYMENT_SOURCE%\%1.config" (
+      %MSBUILD_PATH% "%ESCC_DEPLOYMENT_SCRIPTS%\TransformConfig.xml" /p:TransformInputFile="%DEPLOYMENT_SOURCE%\%1.config" /p:TransformFile="%DEPLOYMENT_TRANSFORMS%%1.Release.config" /p:TransformOutputFile="%DEPLOYMENT_SOURCE%\%1.config"
     )
   )      
 
-  :: Delete temp file created by transformation, because deleting it within the transformation fails due to file locking 
+  REM Delete temp file created by transformation, because deleting it within the transformation fails due to file locking 
   if exist "%DEPLOYMENT_SOURCE%\%1.config.temp.config" (
     del "%DEPLOYMENT_SOURCE%\%1.config.temp.config" 
   )
