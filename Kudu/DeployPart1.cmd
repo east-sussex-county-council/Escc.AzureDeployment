2146552bb8dc5c768363e4d015ce7200c7d1866c
Rick Mason <richard.mason@eastsussex.gov.uk>
2015-10-06 11:04:10 +0100

    Replace tagMapping transform, which worked in test tool but not with MSBuild

--- Kudu/DeployPart1.cmd
+++ Kudu/DeployPart1.cmd
@@ -24,7 +24,7 @@ IF NOT DEFINED DEPLOYMENT_SOURCE (
 )
 
 :: Download deployment scripts
-call Kudu\GitDownload Escc.AzureDeployment v3.2.2
+call Kudu\GitDownload Escc.AzureDeployment v3.2.3
 IF !ERRORLEVEL! NEQ 0 goto error
 
 :: Pass control to a script just downloaded
