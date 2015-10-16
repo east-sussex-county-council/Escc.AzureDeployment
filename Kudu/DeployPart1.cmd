a2c9cf7136248a5820d7c38c127432b56dbe4b48
Rick Mason <richard.mason@eastsussex.gov.uk>
2015-10-16 13:01:15 +0100

    Create .config if missing when setting up application below Umbraco

--- Kudu/DeployPart1.cmd
+++ Kudu/DeployPart1.cmd
@@ -24,7 +24,7 @@ IF NOT DEFINED DEPLOYMENT_SOURCE (
 )
 
 :: Download deployment scripts
-call Kudu\GitDownload Escc.AzureDeployment v3.2.4
+call Kudu\GitDownload Escc.AzureDeployment v3.2.5
 IF !ERRORLEVEL! NEQ 0 goto error
 
 :: Pass control to a script just downloaded
--- Kudu/TransformConfigToWorkBelowUmbraco.cmd
+++ Kudu/TransformConfigToWorkBelowUmbraco.cmd
@@ -13,9 +13,12 @@ echo Transforming %1.config to work below Umbraco
 echo ------------------------------------------------------
 echo.
 
-if exist "%DEPLOYMENT_SOURCE%\%1.config" (
-  "%MSBUILD_PATH%" "%ESCC_DEPLOYMENT_SCRIPTS%\TransformConfig.xml" /p:TransformInputFile="%DEPLOYMENT_SOURCE%\%1.config" /p:TransformFile="%DEPLOYMENT_SOURCE%\Escc.AzureDeployment\Kudu\TransformConfigToWorkBelowUmbraco.xdt" /p:TransformOutputFile="%DEPLOYMENT_SOURCE%\%1.config"
+if not exist "%DEPLOYMENT_SOURCE%\%1.config" (
+  echo Creating %1.config
+  echo ^<?xml version="1.0" encoding="utf-8"?^>^<configuration^>^</configuration^> > "%DEPLOYMENT_SOURCE%\%1.config"
 )
+
+"%MSBUILD_PATH%" "%ESCC_DEPLOYMENT_SCRIPTS%\TransformConfig.xml" /p:TransformInputFile="%DEPLOYMENT_SOURCE%\%1.config" /p:TransformFile="%DEPLOYMENT_SOURCE%\Escc.AzureDeployment\Kudu\TransformConfigToWorkBelowUmbraco.xdt" /p:TransformOutputFile="%DEPLOYMENT_SOURCE%\%1.config"
 
 :: Delete temp file created by transformation, because deleting it within the transformation fails due to file locking 
 if exist "%DEPLOYMENT_SOURCE%\%1.config.temp.config" (
